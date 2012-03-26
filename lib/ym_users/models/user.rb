d083b5f8951b2104de05794823ab01b277e5bb3f
Ian Mooney <ian@yoomee.com>
2012-03-26 17:52:50 +0100

    validate presence of email, password and name

--- lib/ym_users/models/user.rb
+++ lib/ym_users/models/user.rb
@@ -6,6 +6,7 @@ module YmUsers::User
     base.devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
     base.attr_protected :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
+    base.validates_presence_of :email, :password, :first_name, :last_name
     base.image_accessor :image
   end
   
