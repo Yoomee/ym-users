ffc2ea9dc6c452f66b3aac090300f9d9ae23f881
Ian Mooney <ian@yoomee.com>
2012-04-13 13:45:52 +0100

    Added comments to ability template, used in other gems

--- lib/generators/ym_users/templates/models/ability.rb
+++ lib/generators/ym_users/templates/models/ability.rb
@@ -1,12 +1,19 @@
+# Don't delete comments! They are used in gems for adding abilities
 class Ability
+  
   include CanCan::Ability
+  
   def initialize(user)
     if user.try(:admin?)
+      # admin ability
       can :manage, :all      
     elsif user
+      # user ability
       can :manage, User, :id => user.id
     else
+      # open ability
       cannot [:create, :update, :destroy], :all
     end
   end
+  
 end
