54a0f3c442b3f0d173e98e34953393d3169510a5
Edward Andrews <edward@yoomee.com>
2012-07-03 09:19:35 +0100

    Make it possible for users to change password

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "login", :to => "devise/sessions#new", :as => "sign_in"
     delete "logout", :to => "devise/sessions#destroy", :as => "sign_out"
+    get "users/change_password", :to => "devise/registrations#edit", :as => "change_password"
   end
   resources :users
   
--- lib/ym_users/models/user.rb
+++ lib/ym_users/models/user.rb
@@ -3,7 +3,7 @@ module YmUsers::User
   def self.included(base)
     # Include default devise modules. Others available are:
     # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-    base.devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+    base.devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
 
     base.attr_protected :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
     
