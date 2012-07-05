89fc5043dc81734d8d2cdf7ece415f1849798407
Ian Mooney <ian@yoomee.com>
2012-07-05 12:08:06 +0100

    Change factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
 
-  factory :user do |f|
-    f.first_name "Charles"
-    f.last_name "Barrett"
-    f.email "charles@barrett.com"
-    f.password "password"
+  factory :user do
+    first_name "Charles"
+    last_name "Barrett"
+    email {"#{first_name.downcase}@#{last_name.downcase}.com"}
+    password "password"
   end
 
-  factory :admin_user, :parent => :user do |f|
-    f.role 'admin'
+  factory :admin_user, :parent => :user do
+    role 'admin'
   end
 
 end
