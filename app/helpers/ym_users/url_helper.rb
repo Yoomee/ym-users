3c852e7b415ee88e52831ac4db0491e0e79b106b
Ian Mooney <ian@yoomee.com>
2013-04-19 12:34:00 +0100

    Fix for link_if_can

--- app/helpers/ym_users/url_helper.rb
+++ app/helpers/ym_users/url_helper.rb
@@ -4,6 +4,14 @@ module YmUsers::UrlHelper
     options = args.extract_options!
     ability_or_resource = args[block_given? ? 0 : 1]
     if ability_or_resource.is_a?(Array)
+      if ability_or_resource[1].is_a?(Class) && ability_or_resource[1] < ActiveRecord::Base      
+        if Rails.env.development?
+          raise Exception, "Error using CanCan: Don't use can?(:#{ability_or_resource[0]}, #{ability_or_resource[1]}), use can?(:#{ability_or_resource[0]}, #{ability_or_resource[1]}.new) instead"
+        else
+          # this is to make sure can?(:new, Page) is replaced with can?(:new, Page.new)
+          ability_or_resource[1] = ability_or_resource[1].new
+        end
+      end
       return "" unless can?(*ability_or_resource)
       if ability_or_resource[0].to_s == "destroy"
         args[block_given? ? 0 : 1] = ability_or_resource[1]
