d98f3fb512ec1be813d0b844d883fedda47238cc
Edward Andrews <edward@yoomee.com>
2013-04-23 14:53:01 +0100

    Remove trailing spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   devise_scope :user do
     get "login", :to => "devise/sessions#new", :as => "sign_in"
     delete "logout", :to => "devise/sessions#destroy", :as => "sign_out"
-    get "sign-up", :to => "devise/registrations#new", :as => "sign_up" 
+    get "sign-up", :to => "devise/registrations#new", :as => "sign_up"
     get "users/change_password", :to => "devise/registrations#edit", :as => "change_password"
   end
-  resources :users do 
+  resources :users do
     collection do
       get 'stats'
       get 'manage'
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   end
   match 'super' => 'super#index'
   match 'super/morph' => 'super#morph', :via => :post
-  
+
 end
