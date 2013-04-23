e194ac76de3f83fce9414a9ca325b9c6d83dea0c
Edward Andrews <edward@yoomee.com>
2013-04-23 14:54:11 +0100

    Single quotes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   devise_scope :user do
-    get "login", :to => "devise/sessions#new", :as => "sign_in"
-    delete "logout", :to => "devise/sessions#destroy", :as => "sign_out"
-    get "sign-up", :to => "devise/registrations#new", :as => "sign_up"
-    get "change-password", :to => "devise/registrations#edit", :as => "change_password"
+    get 'login', :to => 'devise/sessions#new', :as => 'sign_in'
+    delete 'logout', :to => 'devise/sessions#destroy', :as => 'sign_out'
+    get 'sign-up', :to => 'devise/registrations#new', :as => 'sign_up'
+    get 'change-password', :to => 'devise/registrations#edit', :as => 'change_password'
   end
   resources :users do
     collection do
