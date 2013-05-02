072e47216e3af9a8a6f1a3d39047768f09a180fa
Edward Andrews <edward@yoomee.com>
2013-05-02 16:32:54 +0100

    Allow sign_up_path to be overwritten

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "login", :to => "devise/sessions#new", :as => "sign_in"
     delete "logout", :to => "devise/sessions#destroy", :as => "sign_out"
-    get "sign-up", :to => "devise/registrations#new", :as => "sign_up" 
+    get "sign-up", :to => "devise/registrations#new", :as => "sign_up" unless Settings.dont_set_sign_up_route == true
     get "users/change_password", :to => "devise/registrations#edit", :as => "change_password"
   end
   resources :users do 
