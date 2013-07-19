5e6cee4ed3a28aa530a8a8c566ebb561991f7094
Ian Mooney <ian@yoomee.com>
2013-07-19 11:53:57 +0100

    Add ym_users_routes to routes in install generator

--- lib/generators/ym_users/install_generator.rb
+++ lib/generators/ym_users/install_generator.rb
@@ -1,7 +1,7 @@
 module YmUsers
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+      include YmCore::Generators::Migration
 
       source_root File.expand_path("../templates", __FILE__)
       desc "Install YmUsers"
@@ -9,10 +9,10 @@ module YmUsers
       def manifest
         copy_file "controllers/users_controller.rb", "app/controllers/users_controller.rb"
         copy_file "models/ability.rb", "app/models/ability.rb"
-        if File.exists?("db/seeds.rb")
+        if File.exists?("db/seeds.rb") && !file_contains?("#{Rails.root}/db/seeds.rb", '@yoomee.com')
           append_to_file "db/seeds.rb", File.read(find_in_source_paths("db/seeds.rb"))
         end
-        if File.exists?("app/views/admin/index.html.haml")
+        if File.exists?("app/views/admin/index.html.haml") && !file_contains?("#{Rails.root}/app/views/admin/index.html.haml", 'Users')
           append_to_file "app/views/admin/index.html.haml", File.read(find_in_source_paths("views/admin/index.html.haml"))
         end
         if Dir.glob("public/dragonfly/defaults/user.*").empty?
@@ -23,24 +23,14 @@ module YmUsers
           file_name = file_path.split("/").last
           try_migration_template "migrations/#{file_name}", "db/migrate/#{file_name.sub(/^\d+\_/, '')}"
         end
-      end
-
-      def self.next_migration_number(path)
-        unless @prev_migration_nr
-          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-        else
-          @prev_migration_nr += 1
+        unless file_contains?("#{Rails.root}/config/routes.rb", 'ym_users_routes')
+          route 'ym_users_routes'
         end
-        @prev_migration_nr.to_s
       end
       
       private
-      def try_migration_template(source, destination)
-        begin
-          migration_template source, destination
-        rescue Rails::Generators::Error => e
-          puts e
-        end
+      def file_contains?(file_name, text)
+        File.open(file_name).read.include?(text)
       end
       
     end
