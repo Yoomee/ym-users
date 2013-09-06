7e3116f8fa8ccf8dc25a35dadf1d542533562ba1
Edward Andrews-Hodgson <edward@yoomee.com>
2013-09-06 15:45:54 +0100

    Move email, name and password validations to project model

--- lib/ym_users/models/user.rb
+++ lib/ym_users/models/user.rb
@@ -7,10 +7,7 @@ module YmUsers::User
     base.devise :database_authenticatable, :recoverable, :rememberable, :trackable, :registerable
 
     base.attr_protected :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
-    
-    base.validates_presence_of :email, :first_name, :last_name
-    base.validates :email, :email => true, :uniqueness => true, :allow_blank => true
-    base.validates_presence_of :password, :on => :create
+
     base.image_accessor :image
     base.send(:validates_property, :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif], :case_sensitive => false, :message => "must be an image")
     base.extend(ClassMethods)
