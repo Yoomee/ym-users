55dc3546f114b2ef7e439ddfd496eb9371bf2ce9
Edward Andrews-Hodgson <edward@yoomee.com>
2013-09-12 15:24:27 +0100

    Add reset_password_path

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     delete 'logout', :to => 'devise/sessions#destroy', :as => 'sign_out'
     get 'sign-up', :to => 'devise/registrations#new', :as => 'sign_up'
     get 'change-password', :to => 'devise/registrations#edit', :as => 'change_password'
+    get 'reset-password', :to => 'devise/passwords#new', :as => 'reset_password'
   end
   resources :users do
     collection do
