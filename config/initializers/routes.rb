ebf3aee6249e83038b9405c53fd85bda6438ef3b
Ian Mooney <ian@yoomee.com>
2013-10-02 13:47:06 +0100

    Fix routes

--- config/initializers/routes.rb
+++ config/initializers/routes.rb
@@ -7,21 +7,23 @@ module ActionDispatch::Routing
       if options[:devise]
         devise_for :users
         devise_scope :user do
-          get "login", :to => "devise/sessions#new", :as => "sign_in"
-          delete "logout", :to => "devise/sessions#destroy", :as => "sign_out"
-          get "sign-up", :to => "devise/registrations#new", :as => "sign_up" unless Settings.dont_set_sign_up_route == true
-          get "users/change_password", :to => "devise/registrations#edit", :as => "change_password"
+          get 'login', :to => 'devise/sessions#new', :as => 'sign_in'
+          delete 'logout', :to => 'devise/sessions#destroy', :as => 'sign_out'
+          get 'sign-up', :to => 'devise/registrations#new', :as => 'sign_up'
+          get 'change-password', :to => 'devise/registrations#edit', :as => 'change_password'
+          get 'reset-password', :to => 'devise/passwords#new', :as => 'reset_password'
         end
       end
-      resources :users do 
+
+      resources :users do
         collection do
           get 'stats'
           get 'manage'
+          post 'create_user'
         end
       end
       match 'super' => 'super#index'
       match 'super/morph' => 'super#morph', :via => :post
-      
     end
   
   end
